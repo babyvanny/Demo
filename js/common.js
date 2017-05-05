a3d7e555f50fdfd28db410a99ab80ba56a708a40
vanny <290288575@qq.com>
2017-05-05 16:18:29 +0800

    love u

--- js/common.js
+++ js/common.js
@@ -8,6 +8,7 @@ document.body.onload = window.onresize = function () {
     adjustWith(320);
 }
 
+//链接
 $(".index-go>img").click(function(){
 
       window.location.href="arts.html"
@@ -17,3 +18,29 @@ $(".arts-go>img").click(function(){
 
       window.location.href="designerInfo.html"
 })
+
+//第三个页面，点赞
+
+$(".detail_c").click(function(){
+    var heart_n = parseInt($(".heart_n").text());
+    if($(this).attr("src")=="img/Likes.png"){
+
+       $(this).attr("src","img/Heart-fill.png");
+       $(".heart_n").text(heart_n+1)
+
+    }else{
+        $(this).attr("src","img/Likes.png");
+         $(".heart_n").text(heart_n)
+
+    }
+})
+//第三个页面评论滑动的关闭按钮
+$(".comment_b").click(function(){
+    var bucket_n = parseInt($(".bucket_n").text());
+    var msg_n = parseInt($(".msg_n").text());
+    $(this).parents(".comment").remove();
+    $(".bucket_n").text(bucket_n+1);
+    $(".msg_n").text(msg_n-1);
+
+})
+
